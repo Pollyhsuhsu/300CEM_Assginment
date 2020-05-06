2247d1fb0b8b143671f8e897dc35c1b3b39f9f9b
Pollyhsuhsu <hsu841681209@gmail.com>
2020-05-07 03:14:25 +0800

    wearable

---
+++ app/src/main/java/com/example/a300cem_android_assignment/Wareable/Wareable.java
@@ -0,0 +1,16 @@
+package com.example.a300cem_android_assignment.Wareable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.a300cem_android_assignment.R;
+
+public class Wareable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wareable);
+    }
+}
